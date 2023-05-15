120ee58444a7b11d8443ee97c1167b4ded1cd677
TheWarrior09 <the.warrior.0072@gmail.com>
2023-05-15 15:48:53 +0500

    Add middleware and routes in express app

--- apps/faucet-api/src/main.ts
+++ apps/faucet-api/src/main.ts
@@ -1,13 +1,27 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import { invalidPathHandler } from './middleware';
+import { creditRouter, statusRouter } from './routes';
 
 const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.FAUCET_API_PORT
+  ? Number(process.env.FAUCET_API_PORT)
+  : 9000;
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+  res.send({ message: 'Hello Faucet API' });
 });
+
+app.use('/credit', creditRouter);
+app.use('/status', statusRouter);
+
+app.use(invalidPathHandler);
 
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
