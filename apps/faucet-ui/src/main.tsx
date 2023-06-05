bfe12fd1e99c8def956b8fe6ec23986eb3d3861b
TheWarrior09 <the.warrior.0072@gmail.com>
2023-06-05 17:34:08 +0500

    Add material ui and react-query defaults in main.tsx

--- apps/faucet-ui/src/main.tsx
+++ apps/faucet-ui/src/main.tsx
@@ -1,13 +1,19 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-
+import { CssBaseline } from '@mui/material';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './app/app';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <CssBaseline />
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
