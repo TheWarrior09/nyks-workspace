773b18d8abad470fc1ab313a8d9a912a90d43fdb
TheWarrior09 <the.warrior.0072@gmail.com>
2023-08-30 14:56:33 +0500

    Update proto generated files export

--- libs/proto-ts/src/index.ts
+++ libs/proto-ts/src/index.ts
@@ -1,3 +1,5 @@
 export * from './lib/codegen';
 export * from './lib/codegen/nyks/bridge/tx';
 export * from './lib/codegen/nyks/forks/tx';
+export * from './lib/codegen/nyks/volt/tx';
+export * from './lib/codegen/nyks/zkos/tx';
