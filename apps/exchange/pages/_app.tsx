687c8a983cf5f3cdafd25d4d23aec423f888bc43
TheWarrior09 <the.warrior.0072@gmail.com>
2023-08-30 14:40:25 +0500

    Add _app and _document  pages

---
+++ apps/exchange/pages/_app.tsx
@@ -0,0 +1,78 @@
+import { StrictMode } from 'react';
+import { AppProps } from 'next/app';
+import router from 'next/router';
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { StyledNavbar } from '@nyks-workspace/shared-ui';
+import theme from '../theme';
+import {
+  EnvironmentContextProvider,
+  GlobalContextProvider,
+  useGlobalContext,
+} from '../src/context';
+import { CHAIN_ID, COSMOS_REST, TENDERMINT_RPC } from '../constants';
+
+const queryClient = new QueryClient();
+
+interface MenuItem {
+  label: string;
+  onClick: () => void;
+}
+
+const menuItems: MenuItem[] = [
+  {
+    label: 'Exchange',
+    onClick: () => {
+      router.push(`/trade`);
+    },
+  },
+  {
+    label: 'Testnet Explorer',
+    onClick: () => {
+      router.push(`/explorer`);
+    },
+  },
+  {
+    label: 'Lend Page',
+    onClick: () => {
+      router.push(`/lend`);
+    },
+  },
+];
+
+function CustomApp({ Component, pageProps }: AppProps) {
+  return (
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <GlobalContextProvider>
+            <CssBaseline />
+            <main className="app">
+              <EnvironmentContextProvider>
+                <Navbar />
+                <Component {...pageProps} />
+              </EnvironmentContextProvider>
+            </main>
+          </GlobalContextProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </StrictMode>
+  );
+}
+
+export default CustomApp;
+
+const Navbar = () => {
+  const { hexAddress } = useGlobalContext();
+
+  return (
+    <StyledNavbar
+      menu={menuItems}
+      chainId={CHAIN_ID}
+      cosmosRest={COSMOS_REST}
+      tendermintRpc={TENDERMINT_RPC}
+      hexAddress={hexAddress ?? ''}
+    />
+  );
+};
