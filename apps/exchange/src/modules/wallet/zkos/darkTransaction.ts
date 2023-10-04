78112f26b71e067b1c017ebe443b3c770200b86a
TheWarrior09 <the.warrior.0072@gmail.com>
2023-10-04 15:29:17 +0500

    Create dark transaction using single sender and receiver

--- apps/exchange/src/modules/wallet/zkos/darkTransaction.ts
+++ apps/exchange/src/modules/wallet/zkos/darkTransaction.ts
@@ -116,6 +116,100 @@ export async function darkTransaction({
       height: 0,
     }))
   );
+
+export async function darkTransactionSingle({
+  amountSend,
+  amountAvailable,
+  signature,
+  fromAddress,
+  toAddress,
+  toAddressType,
+  twilightAddress,
+}: {
+  signature: string;
+  twilightAddress: string;
+  fromAddress: string;
+  toAddress: string;
+  toAddressType: 'address' | 'output';
+  amountAvailable: number;
+  amountSend: number;
+}) {
+  const zkos = await import('zkos-wasm');
+
+  const utxos = await getUtxoForAddress(fromAddress);
+
+  const utxoString = JSON.stringify(utxos.result[0]);
+
+  const utxoHex = await getUtxoHex(utxoString);
+
+  const output = await getUtxoOutput(utxoHex);
+
+  const outputString = JSON.stringify(output.result);
+
+  const coinTypeInput = zkos.createInputFromOutput(
+    outputString,
+    utxoString,
+    BigInt(0)
+  );
+
+  let receiver: string;
+
+  if (toAddressType === 'output') {
+    const receiverOutput = await getUtxoOutput(toAddress);
+
+    const receiverOutputString = JSON.stringify(receiverOutput.result);
+
+    const receiverUtxo = zkos.createUtxoFromHex(toAddress);
+
+    receiver = zkos.createInputFromOutput(
+      receiverOutputString,
+      receiverUtxo,
+      BigInt(0)
+    );
+  } else {
+    receiver = toAddress;
+  }
+
+  const darkTxSingle = zkos.darkTransactionSingle(
+    signature,
+    coinTypeInput,
+    receiver,
+    BigInt(amountSend),
+    toAddressType === 'address' ? false : true,
+    BigInt(amountAvailable - amountSend)
+  );
+
+  console.log('darkTxSingle', darkTxSingle);
+
+  const txResponse = await commitDarkTransaction(darkTxSingle);
+
+  const txHash = JSON.parse(txResponse.result).txHash;
+
+  txHash &&
+    updateAccountStatusInLocalData({
+      twilightAddress,
+      tradingAddress: fromAddress,
+    });
+
+  await delay(5000);
+
+  const updatedAddresses = await getUpdatedAddressesFromTx(
+    signature,
+    darkTxSingle
+  );
+
+  AddNewAccountInLocalData(
+    { twilightAddress },
+    JSON.parse(updatedAddresses).map((item: string) => ({
+      tradingAccount: '',
+      encryptScalar: '',
+      tradingAddress: item,
+      transactionId: txHash,
+      transactionType: 'darkTransaction',
+      status: 'unSpend',
+      height: 0,
+    }))
+  );
 }
 
 function delay(ms: number) {
